3ce81a097518a4b210b8fc08c05781564969c69c
Arslan <rslnkrmt2552@gmail.com>
2021-05-03 14:04:19 +0300

    Clean up alchemy_basic_conn

--- alchemy_basic_conn.py
+++ alchemy_basic_conn.py
@@ -3,12 +3,19 @@ import os
 from dotenv import load_dotenv
 from web3 import Web3, WebsocketProvider
 
-load_dotenv()
 
-alchemy_url = os.getenv("ALCHEMY_URL")
+def setup_conn() -> Web3:
+    load_dotenv()
+    alchemy_url = os.getenv("ALCHEMY_URL")
 
-assert alchemy_url is not None
+    assert alchemy_url is not None
 
-w3 = Web3(WebsocketProvider(alchemy_url))
+    w3 = Web3(WebsocketProvider(alchemy_url))
 
-print(w3.isConnected())
+    return w3
+
+
+if __name__ == "__main__":
+    w3 = setup_conn()
+
+    print(f"Connection successful: {w3.isConnected()}")

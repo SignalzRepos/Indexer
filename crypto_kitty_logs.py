8cdceae6b5b0d7df796bdacb2f2388e516c133bc
Arslan <rslnkrmt2552@gmail.com>
2021-05-03 16:35:58 +0300

    Fetch and print crytokitty Dapp tx logs

--- crypto_kitty_logs.py
+++ crypto_kitty_logs.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
+from typing import List
 
 import requests
 from dotenv import load_dotenv
+from web3.datastructures import AttributeDict
 
 from alchemy_basic_conn import get_alchemy_conn
 
@@ -10,9 +12,15 @@ load_dotenv()
 
 CONTRACT_ADDRESS = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"
 abi_filename = "crypto_kitties_abi.txt"
+ERROR_CLR = '\033[91m'
+END_CLR = '\033[0m'
 
 
-def print_cryptokitties_the_difficult_way():
+def print_error(msg: str) -> None:
+    print(f"{ERROR_CLR}{msg}{END_CLR}")
+
+
+def print_cryptokitties_the_difficult_way()->None:
     abi = Path(abi_filename).read_text().strip()
     w3 = get_alchemy_conn()
 
@@ -21,12 +29,15 @@ def print_cryptokitties_the_difficult_way():
     print(f"Contract Name: {contract.functions.name().call()}")
 
 
-def get_k_latest_approved_transaction(k=10):
-    etherscan_api_url = 'https://api.etherscan.io/api'
-    api_key = os.getenv('ETHERSCAN_API_KEY')
+def get_k_latest_approved_transaction(k=10) -> List[str]:
+    etherscan_api_url = "https://api.etherscan.io/api"
+    api_key = os.getenv("ETHERSCAN_API_KEY")
 
     if api_key is None:
-        print("You should add ETHERSCAN_API_KEY to the .env file, or set it as shell env var")
+        print_error(
+            "You should add ETHERSCAN_API_KEY to the .env file, or set it as shell env var"
+        )
+        exit(1)
 
     w3 = get_alchemy_conn()
 
@@ -34,26 +45,52 @@ def get_k_latest_approved_transaction(k=10):
     latest_block_num: int = w3.eth.get_block("latest").number
     print(f"Latest block num -> {latest_block_num}")
 
-    params = {'module': 'account', 'action': 'txlist', 'address': CONTRACT_ADDRESS, 'startblock': 0,
-              'endblock': latest_block_num, 'page': 1, 'offset': k, 'apikey': api_key}
+    params = {
+        "module": "account",
+        "action": "txlist",
+        "address": CONTRACT_ADDRESS,
+        "startblock": 0,
+        "endblock": latest_block_num,
+        "page": 1,
+        "offset": k,
+        "apikey": api_key,
+    }
 
+    print("Fetching transaction list for cryptokitties")
     resp = requests.get(etherscan_api_url, params=params)
 
     if not resp.ok:
-        print("Failed while fetching the transaction list for cryptokitties")
+        print_error("Failed while fetching the transaction list for cryptokitties")
 
     data = resp.json()
-    tx_list = data.get('result')
+    tx_list = data.get("result")
 
     if tx_list is None:
-        print("Seems like we have the wrong response from the tx_list request")
+        print_error("Seems like we have the wrong response from the tx_list request")
 
-    tx_hashes = [tx.get('hash') for tx in tx_list]
+    tx_hashes = [tx.get("hash") for tx in tx_list]
 
     return tx_hashes
 
 
-if __name__ == "__main__":
-    # print_cryptokitties_the_difficult_way()
+def get_crypto_kitty_logs(k=5):
+    w3 = get_alchemy_conn()
+    tx_list = get_k_latest_approved_transaction(k)
 
-    print(get_k_latest_approved_transaction(5))
+    print("Fetching transactions receipts")
+    tx_receipts: List[AttributeDict] = [
+        w3.eth.get_transaction_receipt(tx_hash) for tx_hash in tx_list
+    ]
+
+    tx_logs: List[AttributeDict] = [tx["logs"] for tx in tx_receipts]
+
+    return tx_logs
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    # print_cryptokitties_the_difficult_way()
+    # pprint(get_k_latest_approved_transaction(5))
+
+    pprint(get_crypto_kitty_logs(5))
